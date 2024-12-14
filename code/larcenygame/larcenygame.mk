f8285278abf665a3ea55cf4c93e72d7b582df90b
RiPpEr The Grey Fox <ripper253@gmail.com>
2024-12-14 18:15:58 +0100

    Larceny

---
+++ code/larcenygame/larcenygame.mk
@@ -0,0 +1,20 @@
+
+ASSETS_LIST += \
+	filesystem/larcenygame/map.t3dm \
+	filesystem/larcenygame/collisionSquare.t3dm \
+	filesystem/larcenygame/foxThief.t3dm \
+	filesystem/larcenygame/dogGuard.t3dm \
+	filesystem/larcenygame/dustEffect.t3dm \
+	filesystem/larcenygame/ringObjective.t3dm \
+	filesystem/larcenygame/diamondObjective.t3dm \
+	filesystem/larcenygame/theKilogramObjective.t3dm \
+	filesystem/larcenygame/guardStunHit.wav64 \
+	filesystem/larcenygame/guardSwing.wav64 \
+	filesystem/larcenygame/thiefCaught.wav64 \
+	filesystem/larcenygame/thiefJump.wav64 \
+	filesystem/larcenygame/objectiveTouch.wav64 \
+	filesystem/larcenygame/objectiveGradient.ia8.sprite \
+	filesystem/larcenygame/thiefEye.ci4.sprite \
+	filesystem/larcenygame/floorTest.ci4.sprite \
+	filesystem/larcenygame/wallTest.ci4.sprite \
+	filesystem/larcenygame/drozerix-poppy_flower_girls.xm64
